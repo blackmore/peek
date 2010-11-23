f12b72ca27a60ef684a07e984c585f674b56bf17
Nigel Blackmore <nigel@nigels_macbook.titelbild.de>
2010-11-23 09:50:26 +0100

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Peek::Application.config.secret_token = '14fad552228a7b9549c55ac596b849abf1cee253999f62c7ca2d70ac8fd66d0f3ba93ccb7508f733b9865a825dede8a4bb7e2cf5a68aa3647843040a2f2df3d0'
