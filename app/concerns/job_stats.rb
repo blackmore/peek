d3f8a49f80f61964f7f5778da161d0fdf55f42bb
Nigel Blackmore <nigel@nigels_macbook.titelbild.de>
2010-12-02 13:22:35 +0100

    Doubles hashgit add .

--- app/concerns/job_stats.rb
+++ app/concerns/job_stats.rb
@@ -1,6 +1,6 @@
 module JobStats
 
-  def parse_to_minutes(str)
+  def to_min(str)
     if str
       m = str.split(":")
       m.collect! { |x| x.to_i }
@@ -9,29 +9,61 @@ module JobStats
       0
     end
   end
+  
+  def add_up_minutes(obj, minutes)
+    unless obj.nil?
+      obj += minutes
+    else
+      obj = minutes
+    end
+  end
+  
+  def to_ratio(minutes)
+    unless minutes.nil? || @run_length.nil?
+      (minutes/@run_length.to_f).round(1)
+    end
+  end
 
   def stats
-     @job_run_length = self.description.run_length
+     @run_length = self.description.run_length ||= nil
      
-     total_pr = 0
-     total_dm = 0
-     total_qa = 0
-     total_sub = 0
-     #total_tra = 0
-     total_son = 0
-
+     [@subtitle, @translation, @proof_reading, @quality_assurance, @other].collect { |x| x = nil }
        self.task_times.each do |t|
          case t.job_type
-         when 1 then total_sub += parse_to_minutes(t.duration)
-         #when 2 then total_tra += parse_to_minutes(t.duration)
-         when 3 then total_pr += parse_to_minutes(t.duration)
-         when 4 then total_qa += parse_to_minutes(t.duration)
-         when 5 then total_dm += parse_to_minutes(t.duration)
-         when 6 then total_son += parse_to_minutes(t.duration)
+         when 1 
+           @subtitle = add_up_minutes(@subtitle, to_min(t.duration))
+         when 2
+           @translation = add_up_minutes(@translation, to_min(t.duration))
+         when 3
+           @proof_reading = add_up_minutes(@proof_reading, to_min(t.duration))
+         when 4
+           @quality_assurance = add_up_minutes(@quality_assurance, to_min(t.duration))
+         when 6
+           @other = add_up_minutes(@other, to_min(t.duration))
+         else
+           next
          end
        end
        
-     Hash["r_length" => @job_run_length, "sub_minutes" => total_sub,"pr_minutes" => total_pr, "qa_minutes" => total_qa, "md_minutes" => total_dm, "son_minutes" => total_son ]
+     @statistics = {
+                      'total' => [@subtitle, @translation, @proof_reading, @quality_assurance, @other],
+                      'ratio' => [to_ratio(@subtitle), to_ratio(@translation), to_ratio(@proof_reading), to_ratio(@quality_assurance), to_ratio(@other)],
+                      'run_length' => @run_length
+     }
+     puts "hash: #{@statistics['ratio'][0]}"
+     return @statistics
+     #Hash["r_length" => @job_run_length, "sub_minutes" => @subtitle,"pr_minutes" => @proof_reading, "qa_minutes" => @quality_assurance, "son_minutes" => @other ]
   end
     
 end
+# subtitle
+# translation
+# proof_reading
+# quality_assurance
+# other
+# ratio_subtitle
+# ratio_translation
+# ratio_proof_reading
+# ratio_quality_assurance
+# ratio_other
+# run_length
